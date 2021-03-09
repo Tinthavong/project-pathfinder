532696ac2b6c3d9a572be6cb3c652a7c49134b79
Tinthavong <57294719+Tinthavong@users.noreply.github.com>
2021-03-08 19:21:10 -0500

    More

--- Pathfinder/Assets/Scripts/AStarGrid.cs
+++ Pathfinder/Assets/Scripts/AStarGrid.cs
@@ -8,14 +8,18 @@ public class AStarGrid : MonoBehaviour
     public Vector2 gridWorldSize; //Area of the grid
     public float nodeRadius; //How much space an individual node covers
     public LayerMask unwalkableMask; //Nodes that are unwalkable
-    LayerMask walkableMask; //Layers that are walkable
     public TerrainType[] walkableRegions;
-    AStarNode[,] grid;
+    public int obstacleProximityPenalty = 10;
+
     Dictionary<int, int> walkableRegionsDictionary = new Dictionary<int, int>();
+    LayerMask walkableMask; //Layers that are walkable
+    AStarNode[,] grid;
 
     float nodeDiameter;
     int gridSizeX, gridSizeY;
-
+    int penaltyMin = int.MaxValue;
+    int penaltyMax = int.MinValue;
+    //public int blurSize;
     // Start is called before the first frame update
     void Awake()
     {
@@ -49,19 +53,23 @@ public class AStarGrid : MonoBehaviour
                 int movementPenalty = 0;
 
                 //raycast that compares movementPenalty
-                if (walkable)
+
+                Ray ray = new Ray(worldPoint + Vector3.up * 50, Vector3.down);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit, 100, walkableMask))
                 {
-                    Ray ray = new Ray(worldPoint + Vector3.up * 50, Vector3.down);
-                    RaycastHit hit;
-                    if (Physics.Raycast(ray, out hit, 100, walkableMask))
-                    {
-                        walkableRegionsDictionary.TryGetValue(hit.collider.gameObject.layer, out movementPenalty);
-                    }
+                    walkableRegionsDictionary.TryGetValue(hit.collider.gameObject.layer, out movementPenalty);
+                }
+
+                if (!walkable)
+                {
+                    movementPenalty += obstacleProximityPenalty;
                 }
 
                 grid[x, y] = new AStarNode(walkable, worldPoint, x, y, movementPenalty);
             }
         }
+        BlurPenaltyMap(3);
     }
 
     public List<AStarNode> GetNeighbors(AStarNode node)
@@ -89,6 +97,70 @@ public class AStarGrid : MonoBehaviour
         return neighbors;
     }
 
+    void BlurPenaltyMap(int blurSize)
+    {
+        int kernelSize = blurSize * 2 + 1;// kernel size must be odd numbers
+        int kernelExtents = (kernelSize - 1) / 2;// The limit of the kernel, how many squares are between the central square and the edge of the kernel
+
+        int[,] penaltiesHorizontalPass = new int[gridSizeX, gridSizeY];
+        int[,] penaltiesVerticalPass = new int[gridSizeX, gridSizeY];
+
+        for (int y = 0; y < gridSizeY; y++)
+        {
+            for (int x = -kernelExtents; x <= kernelExtents; x++) //For the first node we need to loop through and sum them up, only the following nodes get to use the formula of: (PreviousSum - 1) - (NextIndex - 1) + (NextIndex + 1)
+            {
+                int sampleX = Mathf.Clamp(x, 0, kernelExtents);
+                //2D array at position 0, y stores the movement penalty
+                penaltiesHorizontalPass[0, y] += grid[sampleX, y].movementPenalty;
+            }
+
+            for (int x = 1; x < gridSizeX; x++)
+            {
+                int removeIndex = Mathf.Clamp(x - kernelExtents - 1, 0, gridSizeX); //index of the node that is no longer inside the kernel as it shifts over
+                int addIndex = Mathf.Clamp(x + kernelExtents, 0, gridSizeX - 1); //node that has just entered the kernel
+
+                //The horizontal value at location x,y is assigned (incremented): 
+                //the previous sum at location [x - 1, y] minus movement penalty value of the previous node grid[removeIndex,y] offset/added by the movement penalty value of the next node grid[addIndex,y]
+                penaltiesHorizontalPass[x, y] = penaltiesHorizontalPass[x - 1, y] - grid[removeIndex, y].movementPenalty + grid[addIndex, y].movementPenalty;
+            }
+        }
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = -kernelExtents; y <= kernelExtents; y++) //For the first node we need to loop through and sum them up, only the following nodes get to use the formula of: (PreviousSum - 1) - (NextIndex - 1) + (NextIndex + 1)
+            {
+                int sampleY = Mathf.Clamp(y, 0, kernelExtents);
+                penaltiesVerticalPass[x, 0] += penaltiesHorizontalPass[x, sampleY]; //Uses the previous horizontal sums found above
+            }
+
+            //Blurs the first row
+            int blurredPenalty = Mathf.RoundToInt((float)penaltiesVerticalPass[x, 0] / (kernelSize * kernelSize));
+            grid[x, 0].movementPenalty = blurredPenalty;
+
+            for (int y = 1; y < gridSizeY; y++)
+            {
+                int removeIndex = Mathf.Clamp(y - kernelExtents - 1, 0, gridSizeY); //index of the node that is no longer inside the kernel as it shifts over
+                int addIndex = Mathf.Clamp(y + kernelExtents, 0, gridSizeY - 1); //node that has just entered the kernel
+
+                //The horizontal value at location x,y is assigned (incremented): 
+                //the previous sum at location [x - 1, y] minus movement penalty value of the previous node grid[removeIndex,y] offset/added by the movement penalty value of the next node grid[addIndex,y]
+                penaltiesVerticalPass[x, y] = penaltiesVerticalPass[x, y - 1] - penaltiesHorizontalPass[x, removeIndex] + penaltiesHorizontalPass[x, addIndex];
+
+                blurredPenalty = Mathf.RoundToInt((float)penaltiesVerticalPass[x, y] / (kernelSize * kernelSize));
+                grid[x, y].movementPenalty = blurredPenalty;
+
+                if (blurredPenalty > penaltyMax)
+                {
+                    penaltyMax = blurredPenalty;
+                }
+                if (blurredPenalty < penaltyMin)
+                {
+                    penaltyMin = blurredPenalty;
+                }
+            }
+        }
+    }
+
     public AStarNode NodeFromWorldPoint(Vector3 worldPosition)
     {
         float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x; //How far along the grid it is (far left 0, middle percentage of 5 and far right is 1)
@@ -109,8 +181,10 @@ public class AStarGrid : MonoBehaviour
         {
             foreach (AStarNode n in grid)
             {
-                Gizmos.color = (n.walkable) ? Color.white : Color.red; //Color is set to white or red if the node is walkable  
-                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
+                Gizmos.color = Color.Lerp(Color.white, Color.black, Mathf.InverseLerp(penaltyMin, penaltyMax, n.movementPenalty));
+
+                Gizmos.color = (n.walkable) ? Gizmos.color : Color.red; //Color is set to white or red if the node is walkable  
+                Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter));
             }
         }
     }
--- Pathfinder/Assets/Scripts/Unit.cs
+++ Pathfinder/Assets/Scripts/Unit.cs
@@ -4,65 +4,104 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+    const float minPathUpdateTime = 0.2f;
+    const float pathUpdateMoveThreshold = 0.5f;
+
     public Transform target;
     public float speed = 20;
-    Vector3[] path;
-    int targetIndex;
+    public float turnDistance = 5;
+    public float turnSpeed = 3;
+    public float stoppingDist = 10;
+
+    Path path;
 
     private void Start()
     {
-        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+        StartCoroutine(UpdatePath());
     }
 
-    public void OnPathFound(Vector3[] newPath, bool pathSuccesssful)
+    public void OnPathFound(Vector3[] waypoints, bool pathSuccesssful)
     {
-        if(pathSuccesssful)
+        if (pathSuccesssful)
         {
-            path = newPath;
+            path = new Path(waypoints, transform.position, turnDistance, stoppingDist);
             StopCoroutine("FollowPath"); //Has to be stopped first in case it is already running
             StartCoroutine("FollowPath");
         }
     }
 
-    IEnumerator FollowPath()
+    //I need a complete refresher on this function
+    IEnumerator UpdatePath()
     {
-        Vector3 currentWayPoint = path[0];
+        if (Time.timeSinceLevelLoad < 0.3f)
+        {
+            yield return new WaitForSeconds(0.3f);
+        }
+        PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
 
+        //squared distance compared with actual distance
+        float squareMoveThreshold = pathUpdateMoveThreshold * pathUpdateMoveThreshold;
+        Vector3 targetPosOld = target.position;
         while (true)
         {
-            if(transform.position == currentWayPoint)
+            yield return new WaitForSeconds(minPathUpdateTime);
+            if ((target.position - targetPosOld).sqrMagnitude > squareMoveThreshold)
             {
-                targetIndex++; //Simply advances to the next waypoint
-                if(targetIndex >= path.Length)
-                {
-                    yield break; //breaks out of coroutine after following is done
-                }
-                currentWayPoint = path[targetIndex]; 
+                PathRequestManager.RequestPath(transform.position, target.position, OnPathFound);
+                targetPosOld = target.position;
             }
-            transform.position = Vector3.MoveTowards(transform.position, currentWayPoint, speed * Time.deltaTime); //current position, target, speed
+        }
+    }
+
+    IEnumerator FollowPath()
+    {
+        bool followingPath = true;
+        int pathIndex = 0;
+        transform.LookAt(path.lookPoints[0]);
+
+        float speedPercent = 1;
+
+        while (followingPath)
+        {
+            Vector2 pos2D = new Vector2(transform.position.x, transform.position.z);
+            while (path.turnBoundaries[pathIndex].HasCrossedLine(pos2D))
+            {
+                if (pathIndex == path.finishLineIndex)
+                {
+                    followingPath = false;
+                    break;
+                }
+                else
+                {
+                    pathIndex++;
+                }
+            }
+
+            if (followingPath)
+            {
+                if (pathIndex >= path.slowDownIndex && stoppingDist > 0)
+                {
+                    speedPercent = Mathf.Clamp01(path.turnBoundaries[path.finishLineIndex].DistanceFromPoint(pos2D) / stoppingDist);
+                    if(speedPercent < 0.01f)
+                    {
+                        followingPath = false;
+                    }
+                }
+
+                Quaternion targetRotation = Quaternion.LookRotation(path.lookPoints[pathIndex] - transform.position);
+                transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * turnSpeed);
+                transform.Translate(Vector3.forward * Time.deltaTime * speed * speedPercent, Space.Self);
+            }
+
             yield return null;
         }
     }
 
     public void OnDrawGizmos()
     {
-        if(path != null)
+        if (path != null)
         {
-            //don't want to draw waypoints passed
-            for(int i = targetIndex; i < path.Length; i++)
-            {
-                Gizmos.color = Color.black;
-                Gizmos.DrawCube(path[i], Vector3.one); //
-
-                if(i == targetIndex)
-                {
-                    Gizmos.DrawLine(transform.position, path[i]);
-                }
-                else
-                {
-                    Gizmos.DrawLine(path[i - 1], path[i]);
-                }
-            }
+            path.DrawWithGizmos();
         }
     }
 }
